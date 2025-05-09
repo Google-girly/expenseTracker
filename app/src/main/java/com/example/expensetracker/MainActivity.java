6b377d8ee5a29ab66230f2a431554b36d61ba372
Victor <victoperez@csumb.edu>
2025-05-09 14:44:20 -0700

    Change the for loop and commented out some lines

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -15,12 +15,18 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     ScrollView scroll;
+    LinearLayout container;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //container = findViewById(R.id.transactionContainer);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -28,37 +34,50 @@ public class MainActivity extends AppCompatActivity {
         });
 
         scroll = (ScrollView) findViewById(R.id.Scroll);
+        container = findViewById(R.id.transactionContainer);
         ExpenseCollection e = new ExpenseCollection(3000);
+
         Category c1 = new Category("Rent", "Red");
         c1.addTransaction(new Transaction("1st half of rent", 700));
         c1.addTransaction(new Transaction("2nd half of rent", 700));
         c1.addTransaction(new Transaction("Light bill", 100));
+
         Category c2 = new Category("Groceries", "Green");
         c2.addTransaction(new Transaction("Fruits and veggies", 50));
         c2.addTransaction(new Transaction("Meat", 100));
         c2.addTransaction(new Transaction("Grains", 30));
+
         Category c3 = new Category("Transportation", "Blue");
         c3.addTransaction(new Transaction("Car payment", 200));
         c3.addTransaction(new Transaction("Gas", 150));
         c3.addTransaction(new Transaction("New air freshener", 10));
+
         e.addCategory(c1);
         e.addCategory(c2);
         e.addCategory(c3);
 
-
-        for(int i = 0; i < e.getCategories().size(); i++){
-            for(int k = 0; k < e.getCategories().get(i).getCount(); k++){
+        for(Category category: e.getCategories()){
+            for(Transaction t : category.getTransactions()){
+                addTransaction(t,category);
+            }
+        }
+        /*
+        for (int i = 0; i < e.getCategories().size(); i++) {
+            for (int k = 0; k < e.getCategories().get(i).getCount(); k++) {
                 addTransaction(e.getCategories().get(i).getTransactions().get(k), e.getCategories().get(i));
             }
         }
 
 
+        ---removed due to it duplicating entries------
         addTransaction(c1.getTransactions().get(0), c1);
         addTransaction(c1.getTransactions().get(0), c1);
 
-
+    */
     }
 
+
+    /*
     private void addTransaction(Transaction t, Category c) {
         LinearLayout ll = new LinearLayout(this);
         scroll.addView(ll);
@@ -71,5 +90,26 @@ public class MainActivity extends AppCompatActivity {
         TextView category = new TextView(this);
         category.setText(c.getName());
         ll.addView(category);
+    }
+}
+
+     */
+    private void addTransaction(Transaction t, Category c) {
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.HORIZONTAL);
+
+        TextView expenseName = new TextView(this);
+        expenseName.setText(t.name);
+        ll.addView(expenseName);
+
+        TextView amount = new TextView(this);
+        amount.setText(String.valueOf(t.getAmount()));
+        ll.addView(amount);
+
+        TextView category = new TextView(this);
+        category.setText(c.getName());
+        ll.addView(category);
+
+        container.addView(ll);
     }
 }
